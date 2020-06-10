e29f2df9da11fe7dcc39296b8366a08dc8db5715
oscarhluviano <oscarhluviano@gmail.com>
2020-06-10 13:34:53 -0500

    Programas Estadistica
    El contenido añadido fueron los programas sobre estadística que vimos en clase

---
+++ PruebasIA/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebasIA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Temas Selectos de Programación 2");
+            Console.WriteLine("*********************************");
+            Console.WriteLine("Grupo: 7");
+            Console.WriteLine("Hernandez Luviano Oscar");
+            Console.WriteLine("*********************************");
+            Console.WriteLine("Tema: Promedio");
+            Console.WriteLine("*********************************");
+            double aux = 0;
+            double error = 0;
+            /*for (int i = 0; i < 100; i++)
+            {
+                aux = Math.Pow(-1, i) * 4 / (2 * i + 1) + aux;          //Método numérico para obtener pi
+                error = 100000 * Math.Abs((Math.PI - aux) / Math.PI);   //Se multiplica el error
+               Console.WriteLine("Pi=" + aux + " Error=" + error);
+            }*/
+            Console.WriteLine();
+        }
+    }
+}
